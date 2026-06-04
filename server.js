6b757b0fa138023ddb84fb7cd026d384dbe49bb9
Pankaj-Lovanshi25 <lovanshipankaj9@gmail.com>
2026-06-04 20:37:21 +0530

    test in api

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 
 const port = 5000;
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Server Running on local system");
 });
 

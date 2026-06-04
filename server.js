c7d6f322948d7867699b8f91e3679f230637f37e
Pankaj-Lovanshi25 <lovanshipankaj9@gmail.com>
2026-06-04 20:29:26 +0530

    server start

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ console.log("hello ")
 console.log("webhook test 2")
 console.log("webhook test 3")
 
+
+const express = require("express");
+const app = express();
+
+const port = 5000;
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+

77310edec46afa315ef54eb98679359cf8192bc0
Pankaj-Lovanshi25 <lovanshipankaj9@gmail.com>
2026-06-05 18:17:11 +0530

    change server port 5004

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (req, res) => {
-  res.send("server is runing  at port 5003");
+  res.send("server is runing  at port 5004");
 });
 
 app.get("/test2" ,(req , res)=>{
   res.send("test 2 is working fine")
 })
 
-app.listen(5003, () => {
-  console.log("Server running on 5003");
+app.listen(5004, () => {
+  console.log("Server running on 5004");
 });
 
 setInterval(() => {

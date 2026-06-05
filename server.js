471518019f6ba9cc4a601e8ae780eaef99faf1d0
Pankaj-Lovanshi25 <lovanshipankaj9@gmail.com>
2026-06-05 15:59:40 +0530

    added test2  route

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ app.get("/test", (req, res) => {
   res.send("server is runing  at port 5002");
 });
 
+app.get("test2" ,(req , res)=>{
+  res.send("test2 is working")
+})
+
 app.listen(5002, () => {
   console.log("Server running on 5002");
 });
+
 
 setInterval(() => {
   console.log("alive");
